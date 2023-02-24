80c719d7d684c5379aa0e6d575f2d85e1cb0772c
GuillaumeDery98 <dery.guillaume98@outlook.fr>
2023-02-24 11:44:52 +0100

    feat:get torrent from Stremio request with Jackett

--- app/Http/Controllers/AddonController.php
+++ app/Http/Controllers/AddonController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Imdb\Title;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Vedmant\FeedReader\Facades\FeedReader;
 
 /**
  * Class addonController
@@ -10,13 +14,20 @@ use Illuminate\Http\Request;
  */
 class AddonController extends Controller
 {
+    public function test()
+    {
+        Log::debug('test');
+        dd($this->stream('movie', 'tt9764362'));
+    }
     /**
      * Summarized collection of meta items. Catalogs are displayed on the Stremio's Board, Discover and Search.
      * @param $type
      * @param $id
      * @return \Illuminate\Http\JsonResponse
      */
-    public function catalog($type, $id, $extra = null) {
+    public function catalog($type, $id, $extra = null)
+    {
+        Log::debug('catalog => type: ' . $type . ' => id : ' . $id . ' => extra : ' . $extra);
         return response()->json([
             "metas" => [
                 [
@@ -39,7 +50,9 @@ class AddonController extends Controller
      * @param $id
      * @return \Illuminate\Http\JsonResponse
      */
-    public function meta($type, $id, $extra = null) {
+    public function meta($type, $id, $extra = null)
+    {
+        Log::debug('meta => type: ' . $type . '=> id: ' . $id . '=> extra: ' . $extra);
         return response()->json([
             "meta" => [
                 "id" => "BigBuckBunny",
@@ -61,15 +74,61 @@ class AddonController extends Controller
      * @param $id
      * @return \Illuminate\Http\JsonResponse
      */
-    public function stream($type, $id, $extra = null) {
+    public function stream($type, $id, $extra = null)
+    {
+
+        Log::debug('stream => type: ' . $type . '=> id: ' . $id . '=> extra: ' . $extra);
+        $infos = $this->getInfos($id);
+        Log::debug($infos["l"]);
+        $name = $infos["l"];
+
         return response()->json([
             "streams" => [
-                [
-                    "availability" => 1,
-                    "url" => "http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_30fps_normal.mp4"
-                ]
+                $this->getTorrent($name, $type)[0]
             ]
         ]);
+    }
+
+    public function getInfos($id)
+    {
+        $client = new Client();
+        $response = end(json_decode($client->request("GET", 'https://imdb-movies-web-series-etc-search.p.rapidapi.com/' . $id . '.json', [
+            'headers' => [
+                'X-RapidAPI-Key' => env('IMDB_API_KEY'),
+                'X-RapidAPI-Host' => 'imdb-movies-web-series-etc-search.p.rapidapi.com'
+            ]
+        ])->getBody()->getContents(), true)["d"]);
+
+        return $response;
+    }
+
+    public function getTorrent($name, $type, $year = null)
+    {
+        $rss = FeedReader::read('http://192.168.1.2:9117/api/v2.0/indexers/yggtorrent/results/torznab?apikey=' . env('JACKETT_API_KEY') . '&t=' . $type . '&q=' . $name);
+        $data = [];
+        /* Full datas 
+        "title" => $item->data['child'][""]["title"][0]['data'],
+                "guid" => $item->data['child'][""]["guid"][0]['data'],
+                "jackettindexer" => $item->data['child'][""]["jackettindexer"][0]['data'],
+                "type" => $item->data['child'][""]["type"][0]['data'],
+                "comments" => $item->data['child'][""]["comments"][0]['data'],
+                "pubDate" => $item->data['child'][""]["pubDate"][0]['data'],
+                "size" => $item->data['child'][""]["size"][0]['data'],
+                "grabs" => $item->data['child'][""]["grabs"][0]['data'],
+                "description" => $item->data['child'][""]["description"][0]['data'],
+                "link" => $item->data['child'][""]["link"][0]['data'],
+                "category" => $item->data['child'][""]["category"][0]['data'],
+                "enclosure" => $item->data['child'][""]["enclosure"][0]['data'],
+        */
+        foreach ($rss->get_items() as $item) {
+            Log::debug("torrent: " . $item->data['child'][""]["link"][0]['data']);
+            $data[] = [
+                "name" => 'YggTorrent',
+                "url" => $item->data['child'][""]["link"][0]['data']
+            ];
+        }
+
+        return $data;
     }
 
     /**
@@ -78,7 +137,8 @@ class AddonController extends Controller
      * @param $id
      * @return \Illuminate\Http\JsonResponse
      */
-    public function subtitles($type, $id, $extra = null) {
+    public function subtitles($type, $id, $extra = null)
+    {
         return response()->json([]);
     }
 
@@ -86,7 +146,8 @@ class AddonController extends Controller
      * Generate a manifest.json file
      * @return \Illuminate\Http\JsonResponse
      */
-    public function manifest() {
+    public function manifest()
+    {
         return response()->json([
             "id" => "org.stremio.laravel",
             "version" => "0.0.1",
